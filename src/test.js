c1fea10d64fc34d7a58a86c695350ff7acc5d95a
Shubhi-03 <mauryashubhi03@gmail.com>
2025-06-26 01:06:32 +0530

    Initial commit

--- src/test.js
+++
@@ -1,7 +0,0 @@
-import { suggest, getSuggestions } from "../src/index.js"
-
-console.log(suggest("javsacript", ["javascript", "java", "typescript"]));
-// => "javascript"
-
-console.log(getSuggestions("gril", ["girl", "grill", "grillz"]));
-// => [ 'girl', 'grill', 'grillz' ]
